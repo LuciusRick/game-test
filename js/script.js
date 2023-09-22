f398694739fbb859e8cb5bf2e81fc85f098b34d6
Lucius Rick Dev <124920048+LuciusRick@users.noreply.github.com>
2023-09-22 18:36:36 -0300

    correction files

--- js/script.js
+++ js/script.js
@@ -1,41 +1,6 @@
-// Variáveis globais para armazenar informações do jogo
-let currentPlayer;
-let opponentPlayer;
-let currentPlayerHealth = 100;
-let opponentPlayerHealth = 100;
-
-function togglePersonaImage() {
-    // Obtém a referência para a div "persona-image"
-    const personaImageDiv = document.getElementById("persona-image");
-
-    // Obtém o estilo atual da div "persona-image"
-    const currentDisplayStyle = getComputedStyle(personaImageDiv).display;
-
-    // Alterna entre "block" e "none" com base no estilo atual
-    if (currentDisplayStyle === "none") {
-        personaImageDiv.style.display = "block";
-    } else {
-        personaImageDiv.style.display = "none";
-    }
-}
-
-function togglePersonaAd() {
-    // Obtém a referência para a div "persona-image"
-    const personaImageDiv = document.getElementById("persona-imagead");
-
-    // Obtém o estilo atual da div "persona-image"
-    const currentDisplayStyle = getComputedStyle(personaImageDiv).display;
-
-    // Alterna entre "block" e "none" com base no estilo atual
-    if (currentDisplayStyle === "none") {
-        personaImageDiv.style.display = "block";
-    } else {
-        personaImageDiv.style.display = "none";
-    }
-}
 
 
 
 
-const suguru = document.querySelector('#suguru-sprite');
+
 
